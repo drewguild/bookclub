09835aff32bccfb6c6ca98df36550540167007e5
drewguild <drew.guild@gmail.com>
2020-01-09 02:49:19 -0500

    load clubs rake task

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,2 +1,11 @@
 class Club < ApplicationRecord
+    has_many :members
+    
+    def create_member!(member_name)
+        first, last = member_name.split(" ")
+        members.create!(
+            first_name: first,
+            last_name: last
+        )
+    end
 end
