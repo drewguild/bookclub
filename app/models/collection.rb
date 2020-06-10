167fe33a67d842ea2d5229a6bc135e9528af0595
drewguild <drew.guild@gmail.com>
2020-06-10 09:58:24 -0400

    books collections join table and model

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,3 +1,5 @@
 class Collection < ActiveRecord::Base
   belongs_to :club
+  has_many :books_collections, class_name: "BooksCollections"
+  has_many :books, through: :books_collections 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200610130959) do
+ActiveRecord::Schema.define(version: 20200610131638) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 20200610130959) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
+  end
+
+  create_table "books_collections", id: false, force: :cascade do |t|
+    t.integer "collection_id", null: false
+    t.integer "book_id", null: false
   end
 
   create_table "clubs", force: :cascade do |t|
