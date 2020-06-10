b86b31831ffafac3e900e1de80cf6a6b965e8726
drewguild <drew.guild@gmail.com>
2020-06-10 12:10:44 -0400

    sync and show thumbnails

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,4 @@
 class Book < ApplicationRecord
   has_many :books_collections, class_name: "BooksCollections"
   has_many :collections, through: :books_collections
-
-  def thumbnail
-
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200610131638) do
+ActiveRecord::Schema.define(version: 20200610155214) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20200610131638) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
+    t.string "thumbnail"
   end
 
   create_table "books_collections", id: false, force: :cascade do |t|
