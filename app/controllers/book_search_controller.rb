13acc7b326b8fdc710b0d8d7ed56916b544b5289
drewguild <drew.guild@gmail.com>
2020-07-31 14:14:13 -0400

    show error message if search fails

--- app/controllers/book_search_controller.rb
+++ app/controllers/book_search_controller.rb
@@ -1,10 +1,12 @@
 class BookSearchController < ApplicationController
   def index
-puts params.inspect
     @book_search_results = Books::Search.new(search_params).call
     # TODO: this template only makes sense in context of nominations page currently. 
     #  Once Nominations are a Collection it can be genericized
     render "index.js.erb"
+
+    rescue StandardError => e
+      render "error.js.erb"
   end
 
   private
