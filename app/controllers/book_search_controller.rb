cdb1bf2f2267f22838362413b043af347ef1bc5a
drewguild <drew.guild@gmail.com>
2020-07-31 13:57:42 -0400

    Use book search for personal list

--- app/controllers/book_search_controller.rb
+++ app/controllers/book_search_controller.rb
@@ -1,7 +1,7 @@
 class BookSearchController < ApplicationController
   def index
+puts params.inspect
     @book_search_results = Books::Search.new(search_params).call
-
     # TODO: this template only makes sense in context of nominations page currently. 
     #  Once Nominations are a Collection it can be genericized
     render "index.js.erb"
